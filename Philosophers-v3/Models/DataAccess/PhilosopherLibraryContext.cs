0893a6774d996455362cd464270d665c07184cec
Stephen Shine <stephen.shine88@gmail.com>
2016-10-18 21:47:49 +0100

    Adding DbContext class

---
+++ Philosophers-v3/Models/DataAccess/PhilosopherLibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Philosophers_v3.Models.Entities;
+
+namespace Philosophers_v3.Models.DataAccess;
+{
+    public class PhilosopherLibraryContext : DbContext
+    {
+        public PhilosopherLibraryContext() : base("PhilosopherLibraryContext") { }
+
+        // Entity set corresponds to table in DB
+        // Row in table is an entity
+        public DbSet<Philosopher> Philosophers { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Nationality> Nationalities { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Philosophers-v3/Models/Entities/Book.cs
+++ Philosophers-v3/Models/Entities/Book.cs
@@ -9,6 +9,8 @@ namespace Philosophers_v3.Models.Entities
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        // Foreign keys are added by naming convention
+        // <naviagtion property name><primary key property name>
         public int PhilosopherID { get; set; }
         public int AreaID { get; set; }
 
