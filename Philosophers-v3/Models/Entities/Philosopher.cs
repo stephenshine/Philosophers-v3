07d99e6da95506e90aa8c455d76e97a685533843
Stephen Shine <stephen.shine88@gmail.com>
2016-10-18 21:25:33 +0100

    Creating philosopher model

---
+++ Philosophers-v3/Models/Entities/Philosopher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Philosophers_v3.Models.Entities
+{
+    public class Philosopher
+    {
+        // will be primary key
+        public int ID { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfDeath { get; set; }
+        public Boolean IsAlive { get; set; }
+        // NationalityID and AreaID will be foreign keys
+        public int NationalityID { get; set; }
+        public int AreaID { get; set; }
+        public string Description { get; set; }
+        
+        // Navigation properties
+        // hold other related entities
+        // defined as virtual to take advantage of lazy loading
+        public virtual Nationality Nationality { get; set; }
+        public virtual Area Area { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+
+    }
+}
